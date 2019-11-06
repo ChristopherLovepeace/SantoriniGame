b86af50bf268bc11cc1da8cb28dbd59f5bd657ad
ChristopherLovepeace <lehristov95@gmail.com>
2019-11-06 15:47:24 +0100

    Our app now has usable modules and routes, also the client can create users

--- modules/db.js
+++ modules/db.js
@@ -1,52 +1,57 @@
-//for database
+//database
 const pg = require("pg");
 
-//Classes in JS
 const db = function(dbConnectionString){
 
-    const connectionString = dbConnectionString;//give me client with that connection string postgres!
+    const connectionString = dbConnectionString;
 
-    async function runQuery(query, params){//general purpose function
-        //connect to db
-        //return info
+    async function runQuery(query, params){
         const client = new pg.Client(connectionString);
         try{
-            await client.connect();//BUT Did I connect?
-            const res = await client.query(query, params);//you shold run queries like this
-            if(res){
-                let response = res.rows[0];//to be fixed
-                //console.log(rs.rows[0].message);
-
-            }else{
-                let response = null;
-            }
+            await client.connect();
+            const res = await client.query(query, params);
+            let response = res.rows[0];
             await client.end();//Must end the connection to the server
             return response;
         }catch(err){
             console.log("And error occured: ", err);
         }
-        
     }
     const getUserByID = async function(userID){
         let userData = null;
         try{
-            userData = await runQuery('SELECT * from UserTbl where userID=$1',[userID]);
+            userData = await runQuery('SELECT * from users where userID=$1',[userID]);
 
         }catch(err){
             console.log(err);
         }
         return userData;//this gives the return a breakpoint
     }
-    const getUserTaskforUser = async function(userID){
-        return await runQuery('SELECT * from UserTasks where userID=$1',[userID]);
-    }
-    return{
-        getuser : getUserByID
+    const createUser = async function(id, email, name, pass){
+        try{
+            let sql = 'INSERT INTO users (id, user_id, user_email, user_name, user_pswhash) VALUES(DEFAULT, $1, $2, $3, $4) RETURNING *';
+            let values = [id, email, name, pass];
+            userData = await runQuery(sql,values);
+
+        }catch(err){
+            console.log(err);
+        }
+        return userData;
     }
 
+    const deleteUser = async function(userID){
+        
+        try{
+
+        }catch(err){
+
+        }
+
+    }
+
+    return {
+        createUser : createUser
+    }
 }
 
 module.exports = db;
-//eyecandy
-class Db{
-}//there should exist only one instance of the db
